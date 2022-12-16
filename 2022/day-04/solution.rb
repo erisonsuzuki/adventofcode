be1b18dfaf96fe37582aea0c673d12ffbf4c4e9c
Erison Suzuki <erisonsuzuki@gmail.com>
2022-12-15 23:50:42 -0300

    refactor: solution for day 4

--- 2022/day-04/solution.rb
+++ 2022/day-04/solution.rb
@@ -1,22 +1,16 @@
-require 'pry'
-
-def part1(first, second)
-  left = (first[0]..first[1]).to_a
-  right = (second[0]..second[1]).to_a
-  intersect = (left & right).size
-
-  intersect == left.size || intersect == right.size
+def part1(left, right)
+  left.cover?(right) || right.cover?(left)
 end
 
-def part2(first, second)
-  left = (first[0]..first[1]).to_a
-  right = (second[0]..second[1]).to_a
-
-  (left & right).any?
+def part2(left, right)
+  left.to_a.intersect? right.to_a
 end
 
 lines = File.readlines('2022/day-04/input.txt').map(&:chomp)
-pairs = lines.map { _1.split(Regexp.union(%w(, -))).each_slice(2).to_a }
+pairs = lines.map do
+  _1.split(Regexp.union(%w[, -])).each_slice(2).to_a
+    .map { |pair| Range.new(*pair) }
+end
 
 p pairs.map { part1(*_1) }.count(true)
-p pairs.map { part2(*_1) }.count(true)
+p pairs.reduce(0) { |count, pair| part2(*pair) ? count + 1 : count }
--- spec/day4_spec.rb
+++ spec/day4_spec.rb
@@ -18,25 +18,25 @@ RSpec.describe 'day 3' do
 
   it do
     array = [
-      ["2-4","6-8", false],
-      ["2-3","4-5", false],
-      ["5-7","7-9", false],
-      ["2-8","3-7", true],
-      ["6-6","4-6", true],
-      ["2-6","4-8", false]
+      ['2-4', '6-8', false],
+      ['2-3', '4-5', false],
+      ['5-7', '7-9', false],
+      ['2-8', '3-7', true],
+      ['6-6', '4-6', true],
+      ['2-6', '4-8', false]
     ].map do |left, right, _result|
-      expect(part1(left.split("-"), right.split("-"))).to(eq(_result))
+      expect(part1(left.split('-'), right.split('-'))).to(eq(_result))
     end
 
     array = [
-      ["2-4","6-8", false],
-      ["2-3","4-5", false],
-      ["5-7","7-9", true],
-      ["2-8","3-7", true],
-      ["6-6","4-6", true],
-      ["2-6","4-8", true]
+      ['2-4', '6-8', false],
+      ['2-3', '4-5', false],
+      ['5-7', '7-9', true],
+      ['2-8', '3-7', true],
+      ['6-6', '4-6', true],
+      ['2-6', '4-8', true]
     ].map do |left, right, _result|
-      expect(part2(left.split("-"), right.split("-"))).to(eq(_result))
+      expect(part2(left.split('-'), right.split('-'))).to(eq(_result))
     end
   end
 end
