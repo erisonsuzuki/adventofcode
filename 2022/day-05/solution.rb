b60608e995caf9ce3a2a5d5ba77a72ef7e580dc2
Erison Suzuki <erisonsuzuki@gmail.com>
2022-12-19 14:39:03 -0300

    refactor: solution of day 5

--- 2022/day-05/solution.rb
+++ 2022/day-05/solution.rb
@@ -18,7 +18,7 @@ def part2(stacks, moves)
     stacks[target - 1].unshift(crates).flatten!
   end
 
-  stacks.map { _1.first }.join('')
+  stacks.map(&:first).join
 end
 
 rows = File.readlines('2022/day-05/input.txt').map(&:chomp)
@@ -27,21 +27,16 @@ lines_list, moves_list = rows.slice_when { _2.empty? }.to_a
 lines = [].tap do |array|
   length = lines_list.pop.split(/[^\d]/).last.to_i
 
-  list = lines_list.map { |line| line.split(/\G {4}/) }.map do |line|
-    crates = [].tap do |crate|
-      line.each do |element|
-        crate << element if element.empty?
-        element.split(Regexp.union(['  ', '] ['])).collect(&:strip).each { crate << _1.gsub(/\W+/, '') }
-      end
-    end
-
-    array << crates.fill('', crates.length, length - crates.length)
+  lines_list.map do |line|
+    crates = line.chars.each_slice(4).map { |col| col.join.scan(/\w/).first }
+    array << crates.fill(nil, crates.length, length - crates.length)
   end
 end
+
 moves = [].tap do |array|
   list = moves_list.map { |move| move.split(Regexp.union(%w[move from to])).reject(&:empty?) }
   list.drop(1).each { array << _1.map(&:to_i) }
 end
 
-p part1(lines.transpose.map { _1.reject(&:empty?) }, moves)
-p part2(lines.transpose.map { _1.reject(&:empty?) }, moves)
+p part1(lines.transpose.map(&:compact), moves)
+p part2(lines.transpose.map(&:compact), moves)
