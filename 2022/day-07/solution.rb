99f541bbb8e3e7bc4fef22cec850d77b6b8a1a63
Erison Suzuki <erisonsuzuki@gmail.com>
2022-12-20 09:45:43 -0300

    feat: solution of day 7

--- 2022/day-07/solution.rb
+++ 2022/day-07/solution.rb
@@ -1,51 +1,39 @@
-def part1(commands)
-  dirs = []
-  {'/' => 0}.tap do |hash|
-    commands.each do |row|
-      command, dir = row.shift.split(" ").drop(1)
+def create_directories(commands)
+  cwd = []
+  directories = Hash.new { |h, k| h[k] = 0 }
 
-      case command
-      when 'cd'
-        dir != '..' ? dirs << dir : dirs.pop
-      when 'ls'
-        row.map { _1.split(" ") }.each do |first, second|
-          if first == 'dir'
-            hash[second] = 0 unless hash[second]
-          else
-            dirs.each { hash[_1] += first.to_i }
-          end
+  commands.each do |row|
+    command, dir = row.shift.split(' ').drop(1)
+
+    case command
+    when 'cd'
+      dir != '..' ? cwd << dir : cwd.pop
+    when 'ls'
+      row.map { _1.split(' ') }.each do |args|
+        next unless args[0] != 'dir'
+
+        cwd.length.times do |x|
+          directories[cwd[0..x]] += args[0].to_i
         end
       end
     end
-  end.values.delete_if { _1 >= 100000 }.sum
+  end
+
+  directories
+end
+
+def part1(commands)
+  create_directories(commands).values.select { _1 <= 100_000 }.sum
+end
+
+def part2(commands)
+  directories = create_directories(commands)
+  necessary_space = 30_000_000 - (70_000_000 - directories[['/']])
+
+  directories.values.select { _1 >= necessary_space }.min
 end
 
 lines = File.readlines('2022/day-07/input.txt').map(&:chomp)
-list = lines.slice_when { _2.include?("$") }.to_a
 
-p part1 list
-
-__END__
-$ cd /
-$ ls
-dir a
-14848514 b.txt
-8504156 c.dat
-dir d
-$ cd a
-$ ls
-dir e
-29116 f
-2557 g
-62596 h.lst
-$ cd e
-$ ls
-584 i
-$ cd ..
-$ cd ..
-$ cd d
-$ ls
-4060174 j
-8033020 d.log
-5626152 d.ext
-7214296 k
+p part1 lines.slice_when { _2.include?('$') }.to_a
+p part2 lines.slice_when { _2.include?('$') }.to_a
