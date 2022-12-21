dcd72186da9ef9e352a540acf13cf752b2489021
Erison Suzuki <erisonsuzuki@gmail.com>
2022-12-21 15:24:20 -0300

    refactor: solution of day 8

--- 2022/day-08/solution.rb
+++ 2022/day-08/solution.rb
@@ -1,30 +1,51 @@
-def part1(array, rows, cols)
+def part1(grid)
   counter = 0
 
-  array.each_with_index do |number, index|
-    # each on the edge
-    if [0, rows - 1].include?(index / cols) || [0, cols - 1].include?(index % cols)
+  grid.map.with_index do |row, i|
+    row.map.with_index do |number, j|
+      if i == 0 || j == 0 || i == grid.length - 1 || j == grid.first.length - 1
+        counter += 1
+        next
+      end
+
+      column = grid.transpose[j]
+
+      if row[(j + 1)..-1].max >= number &&
+         row[0..(j - 1)].max >= number &&
+         column[(i + 1)..-1].max >= number &&
+         column[0..(i - 1)].max >= number
+        next
+      end
+
       counter += 1
-      next
     end
-
-    # find from the directions
-    if Range.new(1, cols - 1 - (index % cols)).to_a.find { |i| array[index + i] >= number } &&
-       Range.new(1, index % cols).to_a.find { |i| array[index - i] >= number } &&
-       Range.new(1, (index / cols)).to_a.find { |i| array[index - (rows * i)] >= number } &&
-       Range.new(1, cols - 1 - (index / cols)).to_a.find { |i| array[index + (rows * i)] >= number }
-      next
-    end
-
-    counter += 1
   end
 
   counter
 end
 
-grid = File.readlines('2022/day-08/input.txt').map(&:chomp).map { _1.chars.map(&:to_i) }
-array = grid.flatten
-rows = grid.length
-cols = grid.first.length
+def part2(grid)
+  counter = 0
 
-p part1(array, rows, cols)
+  grid.map.with_index do |row, i|
+    row.map.with_index do |number, j|
+      next if i == 0 || j == 0 || i == grid.length - 1 || j == grid.first.length - 1
+
+      column = grid.transpose[j]
+
+      trees = [].tap do |tree|
+        tree << (row[(j + 1)..-1].find_index { _1 >= number } || row[(j + 1)..-1].length - 1) + 1
+        tree << (row[0..(j - 1)].reverse.find_index { _1 >= number } || row[0..(j - 1)].length - 1) + 1
+        tree << (column[(i + 1)..-1].find_index { _1 >= number } || column[(i + 1)..-1].length - 1) + 1
+        tree << (column[0..(i - 1)].reverse.find_index { _1 >= number } || column[0..(i - 1)].length - 1) + 1
+      end
+
+      trees.inject(:*)
+    end
+  end
+end
+
+grid = File.readlines('2022/day-08/input.txt').map(&:chomp).map { _1.chars.map(&:to_i) }
+
+p part1(grid)
+p part2(grid).flatten.compact.max
